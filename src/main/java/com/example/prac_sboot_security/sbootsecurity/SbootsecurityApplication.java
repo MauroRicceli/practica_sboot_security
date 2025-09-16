c961d0fa63dd45efb500e9f53bef82fb080b8d5d
mauro <mauroricceli@gmail.com>
2025-09-16 18:40:49 -0300

    initial commit

---
+++ src/main/java/com/example/prac_sboot_security/sbootsecurity/SbootsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.prac_sboot_security.sbootsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootsecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/prac_sboot_security/sbootsecurity/SbootsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prac_sboot_security.sbootsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
